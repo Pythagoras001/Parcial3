092142fc195c75cace0a4a98b35261299feddb1c
PythagorasGit <thomas.gomezr@upb.edu.co>
2026-04-24 20:47:22 -0500

    arreglo detallito ahora si gg ez casi prendo el monitor

--- src/main/java/org/estudio/Services/FileRoutes.java
+++ src/main/java/org/estudio/Services/FileRoutes.java
@@ -2,7 +2,7 @@ package org.estudio.Services;
 
 public class FileRoutes {
     public static String rutaPacientes = "src/main/resources/pacientes.json";
-    public static String rutaDolencias = "src/main/resources/enfermedades.json";
-    public static String rutaEnfermedades = "src/main/resources/dolencias.json";
+    public static String rutaEnfermedades = "src/main/resources/enfermedades.json";
+    public static String rutaDolencias = "src/main/resources/dolencias.json";
 
 }
